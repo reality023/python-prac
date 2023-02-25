8ee9aa5104516d026e49e506a1c93f220217625f
Subong Park <87969561+reality023@users.noreply.github.com>
2023-02-25 06:47:03 +0000

    커밋함

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("test")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def main():
+    return "TEST Code 입니다"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001, debug=True)
